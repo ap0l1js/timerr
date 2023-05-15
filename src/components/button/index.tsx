c40ad9a1dfe6c36201842df7b516de09e39e0c34
Apolinario Manuel <apolinariomanueljs@gmail.com>
2023-05-15 18:42:33 +0100

    feat: route layout

--- src/components/button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './styles';
-
-interface ButtonProperties {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = 'primary' }: ButtonProperties) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-}
--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import { History } from './pages/history';
-import { Home } from './pages/home';
+import { Route, Routes } from "react-router-dom";
+import { DefaultLayout } from "./layout";
+import { History } from "./pages/history";
+import { Home } from "./pages/home";
 
 export function Router() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/history"
-        element={<History />}
-      />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   );
 }
