2ce3e778e24eb455e359c1af6b88113521427721
Apolinario Manuel <apolinariomanueljs@gmail.com>
2023-05-16 14:57:52 +0100

    fix: change interval type

--- src/components/countdown/index.tsx
+++ src/components/countdown/index.tsx
@@ -15,7 +15,7 @@ export function Countdown() {
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
   useEffect(() => {
-    let interval: number;
+    let interval: NodeJS.Timer;
 
     if (activeCycle) {
       interval = setInterval(() => {
